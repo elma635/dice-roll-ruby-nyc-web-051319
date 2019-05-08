e1241cc3cba7274b7e9769ad5bbc428463f10d38
Elma Rahman <elmarahman635@gmail.com>
2019-05-08 06:24:51 +0000

    Automatically backed up by Learn

--- dice_roll.rb
+++ dice_roll.rb
@@ -2,15 +2,13 @@
 
 # Feel free to google "how to generate a random number in ruby"
 #
-# def roll
-#   rand(1..6)
-# end
-#
-# def roll
-#   array.new = {rand(1..6)}
-# end
-
+#setting random num using a range
 def roll
-  array = [1,2,3,4,5,6]
-array[rand(array.length)]
-end 
+  rand(1..6)
+end
+
+#setting random num using an array
+def roll
+  array = [1,2,3,4,5,6] #listing an array = to the set of # in the range
+array[rand(array.length)] #array will call rand(with the lenght of the array) bc its 6 nums
+end
